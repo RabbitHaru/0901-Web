b9904e7a90cf64ebf007baa81671fac92e7b90d0
DESKTOP-S2DD14G\it <rabbitharu1020@gmail.com>
2025-12-02 16:36:12 +0900

    2025-12-02 수업내용 갱신

--- workspace/MustHaveJSP/src/main/java/smtp/NaverSMTP.java
+++ workspace/MustHaveJSP/src/main/java/smtp/NaverSMTP.java
@@ -1,6 +1,5 @@
 package smtp;
 
-
 import java.util.Map;
 import java.util.Properties;
 
@@ -17,37 +16,36 @@ public class NaverSMTP {
 	private final Properties serverInfo;
 	private final Authenticator auth;
 	
-	public NaverSMTP() {
+	// 네이버 smtp서버 접속 설정
+	public NaverSMTP(String id, String pw) {
 		serverInfo = new Properties();
 		serverInfo.put("mail.smtp.host", "smtp.naver.com");
 		serverInfo.put("mail.smtp.port", "465");
-		serverInfo.put("mail.smtp.starttls.enable", "true");
 		serverInfo.put("mail.smtp.auth", "true");
 		serverInfo.put("mail.smtp.debug", "true");
-		serverInfo.put("mail.smtp.socketFactory.prot", "true");
-		serverInfo.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		serverInfo.put("mail.smtp.socketFactory.fallback", "false");
-		
+		serverInfo.put("mail.smtp.ssl.enable", "true");
 		auth = new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("id", "pw");
+				return new PasswordAuthentication(id, pw);
 			}
 		};
-	}
-	
-	public void emailSending(Map<String, String> mailInfo)
-			throws MessagingException{
 		
+	}
+	// 메일을 보내는 메서드
+	public void emailSending(Map<String, String> mailInfo) throws MessagingException {
+		// 네이버 서버에 접속
 		Session session = Session.getInstance(serverInfo, auth);
 		session.setDebug(true);
-		
+		// 이메일 생성
 		MimeMessage msg = new MimeMessage(session);
 		msg.setFrom(new InternetAddress(mailInfo.get("from")));
-		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(mailInfo.get("to")));
+		msg.addRecipient(Message.RecipientType.TO,new InternetAddress(mailInfo.get("to")));
 		msg.setSubject(mailInfo.get("subject"));
 		msg.setContent(mailInfo.get("content"), mailInfo.get("format"));
-		
+		// 메일 전송
 		Transport.send(msg);
 	}
+	
+
 }

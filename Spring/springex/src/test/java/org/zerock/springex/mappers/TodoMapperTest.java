a712acd59b0236867416ade7d663bc3e1508c8b5
DESKTOP-S2DD14G\it <rabbitharu1020@gmail.com>
2025-12-10 16:41:43 +0900

    2025-12-10 수업내용 갱신

---
+++ Spring/springex/src/test/java/org/zerock/springex/mappers/TodoMapperTest.java
@@ -0,0 +1,32 @@
+package org.zerock.springex.mappers;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.springex.vo.TodoVO;
+
+import java.time.LocalDate;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+class TodoMapperTest {
+
+    @Autowired(required = false)
+    private TodoMapper todoMapper;
+
+    @Test
+    void insert() {
+        // builder().설정할 데이터.build() : 객체 생성 방식
+        // 한번 생성한 객체의 데이터를 변경할 수 없음
+        TodoVO vo = TodoVO.builder()
+                .title("스프링 테스트")
+                .dueDate(LocalDate.of(2025,12,10))
+                .writer("user00")
+                .build();
+        todoMapper.insert(vo);
+    }
+}
